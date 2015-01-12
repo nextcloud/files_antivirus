fc01c578ad0f96aa13178c8cbc2e4306252559e8
Lukas Reschke <lukas@owncloud.com>
2015-01-12 13:30:56 +0100

    Remove `ORDER BY` statement
    SQLite seems not to work here with a `ORDER BY` statement. - This prevented updates from previous versions.

--- appinfo/preupdate.php
+++ appinfo/preupdate.php
@@ -5,7 +5,7 @@ if (version_compare($installedVersion, '0.6', '<')) {
 	$query = OC_DB::prepare( 'SELECT COUNT(*) AS `count`, `fileid` FROM `*PREFIX*files_antivirus` GROUP BY `fileid` HAVING COUNT(*) > 1' );
 	$result = $query->execute();
 	while( $row = $result->fetchRow()) {
-		$deleteQuery = OC_DB::prepare('DELETE FROM `*PREFIX*files_antivirus` WHERE `fileid` = ? ORDER BY `check_time` ASC', $row['count']-1);
+		$deleteQuery = OC_DB::prepare('DELETE FROM `*PREFIX*files_antivirus` WHERE `fileid` = ?', $row['count']-1);
 		$deleteQuery->execute(array($row['fileid']));
 	}
 }
