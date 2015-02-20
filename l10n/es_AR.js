01835ad102ac0323f288e71ca5b5ddc5f18e3476
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2015-02-20 01:09:13 -0500

    [tx-robot] updated from transifex

--- l10n/es_AR.js
+++ l10n/es_AR.js
@@ -1,6 +1,13 @@
 OC.L10N.register(
     "files_antivirus",
     {
+    "Clean" : "Limpio",
+    "Infected" : "Infectado",
+    "Unchecked" : "Sin revisar",
+    "Scanner exit status" : "Estado de salida del Escáner",
+    "Scanner output" : "Salida del escáner",
+    "Malware detected" : "Malware detectado",
+    "Virus detected! Can't upload the file %s" : "¡Virus detectado! No se puede subir el archivo %s",
     "Greetings {user}," : "Saludos {user},",
     "Sorry, but a malware was detected in a file you tried to upload and it had to be deleted." : "Perdoná, pero malware fue detectado en un archivo que intentaste subir y tuvo que ser borrado.",
     "This email is a notification from {host}. Please, do not reply." : "Este e-mail es una notificación de {host}. Por favor, no lo respondas.",
@@ -22,11 +29,19 @@ OC.L10N.register(
     "Path to clamscan" : "Ruta al ejecutable clamscan.",
     "Path to clamscan executable." : "Ruta al ejecutable clamscan.",
     "Not required in Daemon Mode." : "No es necesario en modo demonio.",
+    "Extra command line options (comma-separated)" : "Opciones extras de linea de comando (separado por comas)",
     "Action for infected files found while scanning" : "Acción para los archivos infectados que fueron encontrados durante la búsqueda",
     "Only log" : "Solo registrar",
     "Delete file" : "Borrar archivo",
     "Save" : "Guardar",
     "Advanced" : "Avanzado",
-    "Description" : "Descripción"
+    "Rules" : "Reglas",
+    "Clear All" : "Limpiar todo",
+    "Reset to defaults" : "Volver a prefijados",
+    "Match by" : "Igual a",
+    "Scanner exit status or signature to search" : "Estado de salida del escáner o firma a buscar",
+    "Description" : "Descripción",
+    "Mark as" : "Marcar como",
+    "Add a rule" : "Arreglar una regla"
 },
 "nplurals=2; plural=(n != 1);");
