28140b507c3ad4188344aee61e85fcf48439a527
Roeland Jago Douma <roeland@famdouma.nl>
2018-03-02 21:18:07 +0100

    Fix routes
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -6,10 +6,12 @@
  */
 
 return [
-		['name' => 'rule#listAll', 'url' => 'settings/rule/listall', 'verb' => 'GET'],
-		['name' => 'rule#clear', 'url' => 'settings/rule/clear', 'verb' => 'POST'],
-		['name' => 'rule#reset', 'url' => 'settings/rule/reset', 'verb' => 'POST'],
-		['name' => 'rule#save', 'url' => 'settings/rule/save', 'verb' => 'POST'],
-		['name' => 'rule#delete', 'url' => 'settings/rule/delete', 'verb' => 'POST'],
-		['name' => 'settings#save', 'url' => 'settings/save', 'verb' => 'POST'],
+	'routes' => [
+		['name' => 'rule#listAll', 'url' => '/settings/rule/listall', 'verb' => 'GET'],
+		['name' => 'rule#clear', 'url' => '/settings/rule/clear', 'verb' => 'POST'],
+		['name' => 'rule#reset', 'url' => '/settings/rule/reset', 'verb' => 'POST'],
+		['name' => 'rule#save', 'url' => '/settings/rule/save', 'verb' => 'POST'],
+		['name' => 'rule#delete', 'url' => '/settings/rule/delete', 'verb' => 'POST'],
+		['name' => 'settings#save', 'url' => '/settings/save', 'verb' => 'POST'],
+	]
 ];
