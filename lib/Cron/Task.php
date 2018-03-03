519368ea206aa98bd2917137c7748ef30d6cabd3
Roeland Jago Douma <roeland@famdouma.nl>
2018-03-03 22:12:46 +0100

    Use proper Backgroundjob DI
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/Cron/Task.php
+++ lib/Cron/Task.php
@@ -9,26 +9,25 @@
 namespace OCA\Files_Antivirus\Cron;
 
 use OC\BackgroundJob\TimedJob;
-use OCA\Files_Antivirus\AppInfo\Application;
+use OCA\Files_Antivirus\BackgroundScanner;
 
 
 class Task extends TimedJob {
 
+	/** @var BackgroundScanner */
+	private $backgroundScanner;
+
 	/**
 	 * sets the correct interval for this timed job
 	 */
-	public function __construct() {
+	public function __construct(BackgroundScanner $backgroundScanner) {
 		// Run once per 15 minutes
 		$this->setInterval(60 * 15);
+
+		$this->backgroundScanner = $backgroundScanner;
 	}
 
 	protected function run($argument) {
-		if (!\OCP\App::isEnabled('files_antivirus')){
-			return;
-		}
-
-		$application = new Application();
-		$container = $application->getContainer();
-		$container->query('BackgroundScanner')->run();
+		$this->backgroundScanner->run();
 	}
 }
