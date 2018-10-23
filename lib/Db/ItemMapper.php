c82de74f727513c702e403a25c36fea911460751
Roeland Jago Douma <roeland@famdouma.nl>
2018-10-23 22:42:09 +0200

    Override insert method to not fetch the id
    Fixes #86
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

--- lib/Db/ItemMapper.php
+++ lib/Db/ItemMapper.php
@@ -44,4 +44,49 @@ class ItemMapper extends Mapper {
 
 		return $entity;
 	}
+
+	/**
+	 * Creates a new entry in the db from an entity
+	 * @param Entity $entity the entity that should be created
+	 * @return Entity the saved entity with the set id
+	 * @since 7.0.0
+	 * @deprecated 14.0.0 Move over to QBMapper
+	 */
+	public function insert(Entity $entity){
+		// get updated fields to save, fields have to be set using a setter to
+		// be saved
+		$properties = $entity->getUpdatedFields();
+		$values = '';
+		$columns = '';
+		$params = [];
+
+		// build the fields
+		$i = 0;
+		foreach($properties as $property => $updated) {
+			$column = $entity->propertyToColumn($property);
+			$getter = 'get' . ucfirst($property);
+
+			$columns .= '`' . $column . '`';
+			$values .= '?';
+
+			// only append colon if there are more entries
+			if($i < count($properties)-1){
+				$columns .= ',';
+				$values .= ',';
+			}
+
+			$params[] = $entity->$getter();
+			$i++;
+
+		}
+
+		$sql = 'INSERT INTO `' . $this->tableName . '`(' .
+			$columns . ') VALUES(' . $values . ')';
+
+		$stmt = $this->execute($sql, $params);
+
+		$stmt->closeCursor();
+
+		return $entity;
+	}
 }
