a183b0817421e26b0097417b23788c494bf50305
Roeland Jago Douma <roeland@famdouma.nl>
2018-10-23 22:13:36 +0200

    Add an ID column to the main files_antivirus table
    Fixes #86
    
    Signed-off-by: Roeland Jago Douma <roeland@famdouma.nl>

---
+++ lib/Migration/Version10401Date20181023200914.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * @copyright Copyright (c) 2018 Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @author Roeland Jago Douma <roeland@famdouma.nl>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Files_Antivirus\Migration;
+
+use Closure;
+use Doctrine\DBAL\Types\Type;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\SimpleMigrationStep;
+use OCP\Migration\IOutput;
+
+class Version10401Date20181023200914 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options) {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+
+		$table = $schema->getTable('files_antivirus');
+		$table->addColumn('id', Type::INTEGER, [
+			'autoincrement' => true,
+			'notnull' => true,
+			'length' => 4,
+			'unsigned' => true,
+		]);
+		$table->dropPrimaryKey();
+		$table->setPrimaryKey(['id']);
+
+		return $schema;
+	}
+
+}
