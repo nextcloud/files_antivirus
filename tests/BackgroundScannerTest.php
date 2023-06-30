df680086789dcad912a2fbcef5fe542cf3fb30a1
Robin Appelman <robin@icewind.nl>
2023-06-30 16:09:27 +0200

    additional background scanner test
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- tests/BackgroundScannerTest.php
+++ tests/BackgroundScannerTest.php
@@ -11,10 +11,20 @@
 namespace OCA\Files_Antivirus\Tests;
 
 use OC\Files\Storage\Temporary;
+use OCA\Files_Antivirus\AppConfig;
 use OCA\Files_Antivirus\BackgroundJob\BackgroundScanner;
+use OCA\Files_Antivirus\ItemFactory;
+use OCA\Files_Antivirus\Scanner\IScanner;
+use OCA\Files_Antivirus\Scanner\ScannerFactory;
+use OCA\Files_Antivirus\Status;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\EventDispatcher\IEventDispatcher;
+use OCP\Files\Config\IUserMountCache;
 use OCP\Files\Folder;
+use OCP\Files\IMimeTypeLoader;
 use OCP\Files\IRootFolder;
 use OCP\IDBConnection;
+use Psr\Log\LoggerInterface;
 use Test\Traits\MountProviderTrait;
 use Test\Traits\UserTrait;
 
@@ -67,6 +77,33 @@ class BackgroundScannerTest extends TestBase {
 		}
 	}
 
+	private function getBackgroundScanner(): BackgroundScanner {
+		$scannerFactory = $this->createMock(ScannerFactory::class);
+		$status = $this->getMockBuilder(Status::class)
+			->disableOriginalConstructor()
+			->onlyMethods(['getNumericStatus', 'getDetails'])->getMock();
+		$status->method('getNumericStatus')->willReturn(Status::SCANRESULT_CLEAN);
+		$status->method('getDetails')->willReturn("");
+		$scanner = $this->createMock(IScanner::class);
+		$scanner->method('scan')
+			->willReturn($status);
+		$scannerFactory->method('getScanner')
+			->willReturn($scanner);
+		return new BackgroundScanner(
+			\OC::$server->get(ITimeFactory::class),
+			$scannerFactory,
+			\OC::$server->get(AppConfig::class),
+			\OC::$server->get(IRootFolder::class),
+			\OC::$server->get(LoggerInterface::class),
+			\OC::$server->get(IDBConnection::class),
+			\OC::$server->get(IMimeTypeLoader::class),
+			\OC::$server->get(ItemFactory::class),
+			\OC::$server->get(IUserMountCache::class),
+			\OC::$server->get(IEventDispatcher::class),
+			false
+		);
+	}
+
 	private function updateScannedTime(int $fileId, int $time) {
 		/** @var IDBConnection $db */
 		$db = \OC::$server->get(IDBConnection::class);
@@ -81,8 +118,7 @@ class BackgroundScannerTest extends TestBase {
 	public function testGetUnscannedFiles() {
 		$this->markAllScanned();
 
-		/** @var BackgroundScanner $scanner */
-		$scanner = \OC::$server->get(BackgroundScanner::class);
+		$scanner = $this->getBackgroundScanner();
 		$newFileId = $this->homeDirectory->newFile("foo", "bar")->getId();
 
 		$outdated = iterator_to_array($scanner->getUnscannedFiles());
@@ -93,8 +129,7 @@ class BackgroundScannerTest extends TestBase {
 		$newFileId = $this->homeDirectory->newFile("foo", "bar")->getId();
 		$this->markAllScanned();
 
-		/** @var BackgroundScanner $scanner */
-		$scanner = \OC::$server->get(BackgroundScanner::class);
+		$scanner = $this->getBackgroundScanner();
 
 		$outdated = iterator_to_array($scanner->getOutdatedFiles());
 		$this->assertEquals([], $outdated);
@@ -102,5 +137,20 @@ class BackgroundScannerTest extends TestBase {
 		$this->updateScannedTime($newFileId, time() - (30 * 24 * 60 * 60));
 		$outdated = iterator_to_array($scanner->getOutdatedFiles());
 		$this->assertEquals([$newFileId], $outdated);
+	}
+
+	public function testTestScanFiles() {
+		$this->markAllScanned();
+
+		$scanner = $this->getBackgroundScanner();
+		$newFileId = $this->homeDirectory->newFile("foo", "bar")->getId();
+
+		$outdated = iterator_to_array($scanner->getUnscannedFiles());
+		$this->assertEquals([$newFileId], $outdated);
+
+		$scanner->scan(PHP_INT_MAX);
+
+		$outdated = iterator_to_array($scanner->getUnscannedFiles());
+		$this->assertEquals([], $outdated);
 	}
 }
