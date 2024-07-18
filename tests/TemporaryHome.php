a23302860fe5f66c882acc0c16a3ace253b537a8
Robin Appelman <robin@icewind.nl>
2024-07-18 14:55:29 +0200

    improve tests for background scanner
    Signed-off-by: Robin Appelman <robin@icewind.nl>

---
+++ tests/TemporaryHome.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileCopyrightText: 2024 Robin Appelman <robin@icewind.nl>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Files_Antivirus\Tests;
+
+class TemporaryHome extends \OC\Files\Storage\Temporary {
+	private string $id;
+
+	public function __construct($arguments = null) {
+		parent::__construct($arguments);
+		$this->id = uniqid();
+	}
+
+	public function getId() {
+		return "home::" . $this->id;
+	}
+
+}
