df5409bd4fd0b0b0ea3ba057a969a61df6608752
Patrick <zero0cool0@users.noreply.github.com>
2024-07-29 19:13:59 +0200

    Avoid selecting the same fileId multiple times
    Signed-off-by: Patrick <zero0cool0@users.noreply.github.com>

--- lib/BackgroundJob/BackgroundScanner.php
+++ lib/BackgroundJob/BackgroundScanner.php
@@ -204,9 +204,9 @@ class BackgroundScanner extends TimedJob {
 		$dirMimeTypeId = $this->mimeTypeLoader->getId(FileInfo::MIMETYPE_FOLDER);
 
 		$query = $this->db->getQueryBuilder();
-		$query->select('fc.fileid')
+		$query->selectDistinct('fc.fileid')
 			->from('filecache', 'fc')
-			->leftJoin('fc', 'storages', 's', $query->expr()->eq('fc.storage', 's.numeric_id'))
+			->innerJoin('fc', 'storages', 's', $query->expr()->eq('fc.storage', 's.numeric_id'))
 			->leftJoin('fc', 'files_antivirus', 'fa', $query->expr()->eq('fc.fileid', 'fa.fileid'))
 			->innerJoin('fc', 'mounts', 'm', $query->expr()->eq('s.numeric_id','m.storage_id'))
 			->where($query->expr()->isNull('fa.fileid'))
