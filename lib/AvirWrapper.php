fb571545a858fb5d32a940ed4a7464f87f11b7f6
Richard Steinmetz <richard@steinmetz.cloud>
2024-10-23 15:33:35 +0200

    chore: fix compatibility of AvirWrapper::file_put_contents()
    Signed-off-by: Richard Steinmetz <richard@steinmetz.cloud>

--- lib/AvirWrapper.php
+++ lib/AvirWrapper.php
@@ -141,7 +141,7 @@ class AvirWrapper extends Wrapper {
 	 * @return false|float|int
 	 * @throws InvalidContentException
 	 */
-	public function file_put_contents($path, $data) {
+	public function file_put_contents(string $path, mixed $data): int|float|false {
 		if ($this->shouldWrap($path)) {
 			$scanner = $this->scannerFactory->getScanner($this->mountPoint . $path);
 			$scanner->initScanner();
